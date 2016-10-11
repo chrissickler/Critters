41d6f53d46163d224dc61c6c789b800c980dc0d7
chrissickler <christopher.sickler@utexas.edu>
2016-10-11 17:28:15 -0500

    CHanging things

--- src/assignment4/Critter1.java
+++ src/assignment4/Critter1.java
@@ -10,7 +10,6 @@ public class Critter1 extends Critter{
 	
 	private int dir;
 	private int[] genes = new int[8];
-	private int[] dadGenes = new int[8];
 	private boolean isPregnant;
 	
 	public Critter1(){
@@ -43,27 +42,12 @@ public class Critter1 extends Critter{
 			dir = genes[Critter.getRandomInt(8)];
 			if(getEnergy() > (8*Params.min_reproduce_energy) && isPregnant == true){
 				int numKittens = Critter.getRandomInt(3);
-				for(int i = 0; i < numKittens; i++){
-					int malePerc = Critter.getRandomInt(2);
-					if(malePerc == 0){
-						Critter1 kitten = new Critter1();
-						for(int k = 0; k < 8; k++){
-							kitten.genes[k] = (this.genes[k] + this.dadGenes[k]) / 2;
-						}
-						reproduce(kitten, Critter.getRandomInt(8));
-					}
-					else{
-						Critter2 kitten = new Critter2();
-						for(int k = 0; k < 8; k++){
-							kitten.genes[k] = (this.genes[k] + this.dadGenes[k]) / 2;
-						}
-						reproduce(kitten, Critter.getRandomInt(8));
-					}
+				for(int i = 0; i < numKittens; i++){					
 					Critter1 kitten = new Critter1();
 					for(int k = 0; k < 8; k++){
-						kitten.genes[k] = (this.genes[k] + this.dadGenes[k]) / 2;
+						kitten.genes[k] = (this.genes[k] * 2) / 2;
 					}
-					reproduce(kitten, Critter.getRandomInt(8));
+					reproduce(kitten, Critter.getRandomInt(8));					
 				}
 			}
 		}
