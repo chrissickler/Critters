6c5ee76d5347ab2d91a602fd5724286823fd80c1
chrissickler <christopher.sickler@utexas.edu>
2016-10-11 17:20:59 -0500

    Critter 1 and 2 Implementationzzzzzzzzzzzzzz

--- src/assignment4/Critter1.java
+++ src/assignment4/Critter1.java
@@ -3,13 +3,22 @@ package assignment4;
  * This critter takes after a cat. It likes to sleep. 
  * When sleeping, it does not lose energy
  * When it is awake, it moves normally
+ * Female Cat
  * @author Chris 
  * */
 public class Critter1 extends Critter{
 	
 	private int dir;
+	private int[] genes = new int[8];
+	private int[] dadGenes = new int[8];
+	private boolean isPregnant;
 	
-	public Critter1(){}
+	public Critter1(){
+		for(int i = 0; i < 8; i++){
+			genes[i] = Critter.getRandomInt(8);
+		}
+		isPregnant = false;
+	}
 
 	public int getDir() {
 		return dir;
@@ -26,17 +35,47 @@ public class Critter1 extends Critter{
 	@Override
 	public void doTimeStep() {
 		int percent = Critter.getRandomInt(100);
-		if(percent < 25){
-			return;
+		if(percent < 30){
+			return;//The cat/critter is asleep because its a cat
+		}
+		else if(percent < 15){
+			walk(dir);			
+			dir = genes[Critter.getRandomInt(8)];
+			if(getEnergy() > (8*Params.min_reproduce_energy) && isPregnant == true){
+				int numKittens = Critter.getRandomInt(3);
+				for(int i = 0; i < numKittens; i++){
+					int malePerc = Critter.getRandomInt(2);
+					if(malePerc == 0){
+						Critter1 kitten = new Critter1();
+						for(int k = 0; k < 8; k++){
+							kitten.genes[k] = (this.genes[k] + this.dadGenes[k]) / 2;
+						}
+						reproduce(kitten, Critter.getRandomInt(8));
+					}
+					else{
+						Critter2 kitten = new Critter2();
+						for(int k = 0; k < 8; k++){
+							kitten.genes[k] = (this.genes[k] + this.dadGenes[k]) / 2;
+						}
+						reproduce(kitten, Critter.getRandomInt(8));
+					}
+					Critter1 kitten = new Critter1();
+					for(int k = 0; k < 8; k++){
+						kitten.genes[k] = (this.genes[k] + this.dadGenes[k]) / 2;
+					}
+					reproduce(kitten, Critter.getRandomInt(8));
+				}
+			}
 		}
 		else{
-			walk(dir);
+			run(dir);
+			dir = genes[Critter.getRandomInt(8)];
 		}
 		
 	}
 
 	@Override
-	public boolean fight(String oponent) {
+	public boolean fight(String opponent) {
 		// TODO Auto-generated method stub
 		return false;
 	}
--- src/assignment4/Critter2.java
+++ src/assignment4/Critter2.java
@@ -1,10 +1,20 @@
 package assignment4;
-
+/**
+ * This Critter is actually a male cat
+ * @author Chris
+ * */
 public class Critter2 extends Critter{
 	
 	private int dir;
+	public int[] genes = new int[8];
+	private boolean stepTaken;
 	
-	public Critter2(){}
+	public Critter2(){
+		for(int i = 0; i < 8; i++){
+			genes[i] = Critter.getRandomInt(8);
+		}
+		stepTaken = false;
+	}
 
 	public int getDir() {
 		return dir;
@@ -23,12 +33,29 @@ public class Critter2 extends Critter{
 	
 	@Override
 	public void doTimeStep() {
-		// TODO Auto-generated method stub
+		int percent = Critter.getRandomInt(100);
+		if(percent < 25){
+			return;//The cat/critter is asleep because its a cat
+		}
+		else if(percent < 5){
+			run(dir);
+		}
+		else{
+			walk(dir);
+			if(stepTaken == false){
+				stepTaken = true;
+			}
+			else{
+				dir = (dir + 2) % 8;
+				stepTaken = false;
+			}
+			
+		}
 		
 	}
 
 	@Override
-	public boolean fight(String oponent) {
+	public boolean fight(String opponent) {
 		// TODO Auto-generated method stub
 		return false;
 	}
